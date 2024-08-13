d1df7ba9e8d5e115a41f94ef69cc2e2be9c16c8a
Abell <abelllfineghan@gmail.com>
2024-08-13 07:23:24 -0300

    models

--- suap/models.py
+++ suap/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    matricula = models.CharField(max_length=10)
+    email = models.EmailField()
+    senha = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
