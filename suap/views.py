ad2d010e020917d6673ebbc067fafc3a4e63dd5d
Abell <abelllfineghan@gmail.com>
2024-08-13 07:33:32 -0300

    views

--- suap/views.py
+++ suap/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Aluno
 # Create your views here.
+def index(request):
+    alunos = Aluno.objects.all()
+    contexto = {
+        'lista': alunos,
+    }
+    return render(request, 'suap/index.html', contexto)
+    
