9c1da69accf3ab818f89d137ed20504ad55f1d7c
Abell <abelllfineghan@gmail.com>
2024-08-13 08:32:50 -0300

    primeiro commit depois da volta

---
+++ suap/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=250)
+    matricula = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
+    
+class Curso(models.Model):
+    nome = models.CharField(max_length=250)
+    carga_horaria = models.IntegerField()
+    descricao = models.TextField()
+    alunos = models.ManyToManyField(Aluno)
+    
+    def __str__(self):
+        return self.nome
+    
