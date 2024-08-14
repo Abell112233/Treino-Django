05bdfa4c5fdb99d056812fbedbe25eaea8aaed6c
Camille Kauane Bezerra da Silva <camille.s@academico.ifrn.edu.br>
2024-08-14 14:17:27 -0300

    mais imagens

--- suap/models.py
+++ suap/models.py
@@ -6,13 +6,17 @@ class Curso(models.Model):
     nome = models.CharField(max_length=250)
     carga_horaria = models.CharField(max_length=50)
     descricao = models.TextField()
+    imagem = models.ImageField(upload_to="curso/", null=True, blank=True)
     
     def __str__(self):
         return self.nome
 
 class Aluno(models.Model):
-    nome = models.CharField(max_length=250)
-    matricula = models.CharField(max_length=50)
+    nome = models.CharField(max_length=250, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    cpf = models.CharField(max_length=11, null=True, blank=True)
+    data_nascimento = models.DateField(null=True, blank=True)
+    foto = models.ImageField(upload_to="fotos/", null=True, blank=True)
     curso = models.ManyToManyField(Curso)
     def __str__(self):
-        return (self.nome + ' - ' + self.matricula)
+        return (self.nome)
