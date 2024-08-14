cf0def689f264e41ad158d8e82114932d6a36ecc
Abell112233 <abelllfineghan@gmail.com>
2024-08-14 08:55:13 -0300

    adição de imagens

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from suap.views import index, Informatica, Alimentos, Apicultura, ADS
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +29,5 @@ urlpatterns = [
     path('apicultura/', Apicultura, name='apicultura'),
     path('ads/', ADS, name='ads'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
